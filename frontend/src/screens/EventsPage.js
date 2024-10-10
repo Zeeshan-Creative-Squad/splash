b015d4d38f9e674bac0b45324aa611c9796d5ae8
Muhammad Zeeshan <zeeshan.creativesquad@gmail.com>
2024-10-10 05:59:28 +0500

    Work in Progress

--- frontend/src/screens/EventsPage.js
+++ frontend/src/screens/EventsPage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Events from '../components/Events/Events'
 import CommonHeroComponent from '../components/CommonHeroComponent/CommonHeroComponent'
 import CardGrid from '../components/CardGrid/CardGrid'
+import EventsSelection from '../components/EventsSelection/EventsSelection'
 
 const EventsPage = () => {
     return (
         <div>
             <CommonHeroComponent title="" Heading="OUR EVENTS" Para="Convallis vivamus at cras porta nibh velit Convallis vivamus at cras porta nibh velit" backgroundImg="/images/creatives/AboutHero-Banner.jpg" />
             <CardGrid />
+            <EventsSelection />
 
             <Events />
         </div>
