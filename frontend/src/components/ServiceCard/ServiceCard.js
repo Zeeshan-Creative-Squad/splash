a295b566010e3f9268eda11b856e056e7f7337d4
Muhammad Zeeshan <zeeshan.creativesquad@gmail.com>
2024-10-30 03:13:07 +0500

    Work Completed

--- frontend/src/components/ServiceCard/ServiceCard.js
+++ frontend/src/components/ServiceCard/ServiceCard.js
@@ -11,46 +11,46 @@ function ServiceCard() {
 
     return (
         <div className='ServiceCard-container'>
-        <div className="container mt-5">
-            <div className="row text-center">
-                <h1 className='head-h1'>THE LATEST NEWS</h1>
-                <p className='para_main'>Splash Sports Events is powered by a small but influential team dedicated to promoting multi-sports for kids.</p>
+            <div className="container mt-5">
+                <div className="row text-center">
+                    <h1 className='head-h1'>THE LATEST NEWS</h1>
+                    <p className='para_main'>Splash Sports Events is powered by a small but influential team dedicated to promoting multi-sports for kids.</p>
 
-                {/* Map over the image list */}
-                {images.map((image, index) => (
-                    <div className="col-md-4" key={index}>
-                        {/* Render image */}
-                        <img
-                            src={image.src}
-                            alt={image.alt}
-                            className={`img-fluid ${index === 2 ? 'third-image' : ''}`}
-                        />
-
-                        {/* Render date if present */}
-                        {image.date && <p className="text-danger mt-3">{image.date}</p>}
-                        {/* Render title if present */}
-                        {image.title && <h4 className='head-h3'>{image.title}</h4>}
-                        {image.date && <p className="text-danger mt-3">{image.dated}</p>}
-                        {image.title && <h4 className='head-h3'>{image.titled}</h4>}
-
-                        {/* Render an additional image beside the third one if it's index 0 or 2 */}
-                        {(index === 0 || index === 2) && (
+                    {/* Map over the image list */}
+                    {images.map((image, index) => (
+                        <div className="col-md-4" key={index}>
+                            {/* Render image */}
                             <img
                                 src={image.src}
                                 alt={image.alt}
-                                className="img-fluid mt-5"
+                                className={`img-fluid ${index === 2 ? 'third-image' : ''}`}
                             />
-                        )}
 
-                    </div>
-                ))}
-            </div>
+                            {/* Render date if present */}
+                            {image.date && <p className="text-danger mt-3">{image.date}</p>}
+                            {/* Render title if present */}
+                            {image.title && <h4 className='head-h3'>{image.title}</h4>}
+                            {image.date && <p className="text-danger mt-3">{image.dated}</p>}
+                            {image.title && <h4 className='head-h3'>{image.titled}</h4>}
 
-            {/* Center large image */}
-            <div className="row text-center mt-5">
-                {/* You can map similarly here for the second row of images */}
+                            {/* Render an additional image beside the third one if it's index 0 or 2 */}
+                            {(index === 0 || index === 2) && (
+                                <img
+                                    src={image.src}
+                                    alt={image.alt}
+                                    className="img-fluid mt-5 d-none d-lg-block"
+                                />
+                            )}
+
+                        </div>
+                    ))}
+                </div>
+
+                {/* Center large image */}
+                <div className="row text-center mt-5">
+                    {/* You can map similarly here for the second row of images */}
+                </div>
             </div>
-        </div>
         </div>
     );
 }
